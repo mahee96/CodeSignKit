0cf78f2ae2f9977a8882a3a508764e54bd0b6685
mahee96 <47920326+mahee96@users.noreply.github.com>
2026-08-31 14:04:36 +0530

    restrict xcframeworks to just macos, ios, visionos, watchos, tvos

--- Package.swift
+++ Package.swift
@@ -2,6 +2,22 @@
 
 import PackageDescription
 
+#if canImport(Darwin)
+let openSSLBinaryTargets: [Target] = [
+    .binaryTarget(
+        name: "OpenSSL",
+        url: "https://github.com/krzyzanowskim/OpenSSL/releases/download/3.6.2000/OpenSSL.xcframework.zip",
+        checksum: "37846a8bd302cb2443eff47f1045ab844d0cd40bf82cc6159cfad9aa5c3eff9e"
+    )
+]
+let openSSLTestDependencies: [Target.Dependency] = [
+    .target(name: "OpenSSL")
+]
+#else
+let openSSLBinaryTargets: [Target] = []
+let openSSLTestDependencies: [Target.Dependency] = []
+#endif
+
 let package = Package(
     name: "CodeSignKit",
     platforms: [
@@ -26,11 +42,6 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-asn1.git",   exact: "1.6.0")
     ],
     targets: [
-        .binaryTarget(
-            name: "OpenSSL",
-            url: "https://github.com/krzyzanowskim/OpenSSL/releases/download/3.6.2000/OpenSSL.xcframework.zip",
-            checksum: "37846a8bd302cb2443eff47f1045ab844d0cd40bf82cc6159cfad9aa5c3eff9e"
-        ),
         .target(
             name: "CodeSignKit",
             dependencies: [
@@ -53,10 +64,9 @@ let package = Package(
                 "CodeSignKit",
                 .product(name: "Crypto",        package: "swift-crypto"),
                 .product(name: "CryptoExtras",  package: "swift-crypto"),
-                .product(name: "SwiftASN1",     package: "swift-asn1"),
-                .target(name: "OpenSSL")
-            ],
+                .product(name: "SwiftASN1",     package: "swift-asn1")
+            ] + openSSLTestDependencies,
             path: "Tests"
         )
-    ]
+    ] + openSSLBinaryTargets
 )
